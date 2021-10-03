8d3b7363ebb14b281e382fb71c733fb3d8728652
MonicaGonzalez30 <monik196901@gmail.com>
2021-10-02 21:03:37 -0500

    Modificaciones en el frontend

---
+++ frontend/js/flujo.js
@@ -0,0 +1,19 @@
+function agregar(){
+    //alert("Se estan agregando columnas")
+
+}
+
+function eliminar(){
+    //alert("Se estan eliminando columnas")
+    
+}
+
+function guardar(){
+    //alert("Se estan agregando columnas")
+    window.location="./index.html"; //Redirigir a la pagina
+}
+
+function cancelar(){
+    alert("Se cancelaron los cambios")
+    window.location="./index.html";
+}
