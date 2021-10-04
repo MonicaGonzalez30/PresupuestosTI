09158d9b0f368096e90109f7efe3510f981dfba6
MonicaGonzalez30 <monik196901@gmail.com>
2021-10-04 12:11:49 -0500

    Solución del error para validar los datos del cambio de contraseña

--- backend/middleware/validationModels/newPass.js
+++ backend/middleware/validationModels/newPass.js
@@ -5,13 +5,7 @@ module.exports = {
         user: Joi.string().alphanum().min(6).max(30).required(),
         password: Joi.string().min(8).max(50).required(),
         newPassword: Joi.string().min(8).max(50).required()
-    }).with('user','password','newPassword')
+    }).with('user',['password','newPassword'])
+    /*.with('user','password')    Permite validar más de 2 datos con varios with
+      .with('password','newPassword')*/
 }
-
-/*const passwordModel = Joi.object().keys({
-    user: Joi.string().alphanum().min(6).max(30).required(),
-    password: Joi.string().min(8).max(50).required(),
-    newPassword: Joi.string().min(8).max(50).required()
-}).with('user','password','newPassword')
-
-module.exports = passwordModel;*/
